7018a086820d4a683c39df52c7631b3fe05c63f2
WesleyCh3n <wesley.ch3n.0530@gmail.com>
2022-08-22 16:25:01 +0800

    🌱 init commit

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import { Menu } from "./Menu";
+
+
+function App() {
+  return (
+    <div className="h-screen flex justify-center items-center bg-orange-100">
+      <div className="w-[332px] h-28 bg-white drop-shadow-2xl">
+        <Menu />
+      </div>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/MenuItem.tsx
@@ -0,0 +1,16 @@
+import { motion, MotionProps } from "framer-motion";
+import { HTMLAttributes } from "react";
+
+const MenuItem = (props: HTMLAttributes<HTMLDivElement> & MotionProps) => {
+  return (
+    <motion.div
+      className="absolute w-12 h-12 bg-gray-200 rounded-full
+                 drop-shadow hover:drop-shadow-lg
+                 flex justify-center items-center"
+      {...props}
+    >
+    </motion.div>
+  );
+};
+
+export default MenuItem;
