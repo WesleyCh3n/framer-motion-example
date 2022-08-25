bfe4528833783ac8ed044026a818e055133b4c39
WesleyCh3n <wesley.ch3n.0530@gmail.com>
2022-08-25 10:39:50 +0800

    ✨ feat: use spring

--- src/ScrollLink/index.tsx
+++ src/ScrollLink/index.tsx
@@ -1,15 +1,19 @@
-import { motion, useScroll } from "framer-motion";
+import { motion, useScroll, useSpring } from "framer-motion";
 import { useRef } from "react";
 import LoremIpsum from "./LoremIpsum";
 
 const ScrollLink = () => {
   const ref = useRef(null);
-
   const { scrollYProgress } = useScroll({ container: ref });
+  const scaleX = useSpring(scrollYProgress);
 
   return (
-    <div className="w-full h-full overflow-y-scroll" ref={ref} >
-      <motion.div className="fixed bottom-0 right-0 left-0 h-[10px] bg-orange-400 origin-[0%]" style={{ scaleX: scrollYProgress }} />
+    <div className="w-full h-full overflow-y-scroll" ref={ref}>
+      <motion.div
+        className="fixed bottom-0 right-[5px] left-0 h-[10px] bg-blue-400
+        origin-[0%]"
+        style={{ scaleX }}
+      />
       <LoremIpsum />
     </div>
   );
